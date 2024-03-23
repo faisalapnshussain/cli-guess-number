cd4de876a2700bf6c7f3e15779e80e782216045f
Faisal Hussain <faisalapnshussain@gmail.com>
2024-03-23 12:45:35 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random () * 10 +1);
+
+const result = await inquirer.prompt([
+{name:"guessNumber",
+type:"number",
+message:"Guess a number from 1-10"}
+]);
+
+if (result.guessNumber === randomNumber){
+    console.log("Your Guess is Right, You won");
+} else {
+    console.log("Try Again");
+}
